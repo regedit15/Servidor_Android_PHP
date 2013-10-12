2cda2d694f6d088709673f36d881d653af742bb3
regedit15 <001regedit15@gmail.com>
2013-10-12 18:06:30 -0300

    se modifico el listar productos. quedo andando

--- listarProductos.php
+++ listarProductos.php
@@ -1,14 +1,32 @@
 <?php
 
-	include('functions.php'); 
+include('functions.php');
 
-	if ($resultset = getSQLResultSet("SELECT * FROM  `prueba`.`productos`")) 
-	{	
-    	while ($row = $resultset->fetch_array(MYSQLI_NUM)) 
-    	{
-    		echo json_encode($row);    	
-    	}    	
-   }  
+$host="localhost"; //replace with database hostname 
+$username="root"; //replace with database username 
+$password=""; //replace with database password 
+$db_name="prueba"; //replace with database name
 
-?>
 
+
+$con=mysql_connect("$host", "$username", "$password")or die("cannot connect"); 
+mysql_select_db("$db_name")or die("cannot select DB");
+ 
+
+$sql = "SELECT * FROM  `prueba`.`productos`"; 
+$result = mysql_query($sql);
+$json = array();
+ 
+if(mysql_num_rows($result))
+{
+	while($row = mysql_fetch_assoc($result))
+	{
+		$json['lista_productos'][]=$row;
+	}
+}
+mysql_close($con);
+echo json_encode($json); 
+
+
+
+?> 
